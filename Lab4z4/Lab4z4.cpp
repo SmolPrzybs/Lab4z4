db2ef31ff035fdc3717694ddbbb92d67508b1609
SmolPrzybs <maciek970@gmail.com>
2022-01-21 20:27:37 +0100

    Dodaj pliki projektów.

---
+++ Lab4z4/Lab4z4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+long  dwusilnia( int n)
+{
+	long  ds = 1; //zmienna przechowująca kolejne mnożenia
+	for (; n > 1; n -= 2)
+		ds = ds* n;
+	return ds;
+}
+
+int main()
+{
+	int n;
+	cout << "Podaj liczbe: ";
+	cin >> n;
+	cout << "Dwusilnia z liczby " << n << " wynosi " << dwusilnia(n) << endl;
+
+	return 0;
+}
